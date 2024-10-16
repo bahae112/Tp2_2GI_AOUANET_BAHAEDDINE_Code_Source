162c53e054f5bc1f0cf5b08d7bbc512a4d432ebd
bahae112 <151392316+bahae112@users.noreply.github.com>
2024-10-16 17:01:25 +0200

    Update PaiementCarteBancaire.java

--- src/PaiementCarteBancaire.java
+++ src/PaiementCarteBancaire.java
@@ -1,7 +1,6 @@
 class PaiementCarteBancaire implements Paiement {
     @Override
     public void effectuerPaiement(double montant) {
-        System.out.println("Paiement de " + montant + "€ effectué par carte bancaire.");
-        // Logique spécifique à la carte bancaire
+        System.out.println("Paiement de " + montant + " effectué par carte bancaire.");
     }
 }
